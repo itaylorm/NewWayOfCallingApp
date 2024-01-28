0aa665f79d320c66716382eb675be2e6133ccd6e
Taylor Maxwell <taylor@imaxwell.net>
2024-01-28 12:03:06 -0800

    Add project files.

---
+++ NewWayOfCalling/Benchmarks.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Reflection;
+
+namespace NewWayOfCalling;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private readonly Example _example = new();
+
+    private static readonly MethodInfo CachedMethod = typeof(Example)
+        .GetMethod("Method", BindingFlags.NonPublic | BindingFlags.Instance)!;
+
+    [Benchmark]
+    public string GetMethod_Reflection()
+    {
+        return (string)_example.GetType()
+            .GetMethod("Method", BindingFlags.NonPublic | BindingFlags.Instance)!
+            .Invoke(_example, null)!;
+    }
+
+    [Benchmark]
+    public string GetMethod_ReflectionCached()
+    {
+        return (string)CachedMethod.Invoke(_example, null)!;
+    }
+
+    [Benchmark]
+    public string GetMethod_Unsafe()
+    {
+        return Caller.CallMethod(_example);
+    }
+
+    [Benchmark]
+    public string GetMethod_Direct()
+    {
+        return Caller.PublicMethod();
+    }
+}
---
+++ NewWayOfCalling/Program.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Running;
+using NewWayOfCalling;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+//var example = new Example();
+
+//// old way
+//var method = typeof(Example).GetMethod("Method", BindingFlags.NonPublic | BindingFlags.Instance);
+//Console.WriteLine(method.Invoke(example, null));
+
+//// new way
+//var name = Caller.CallMethod(example);
+//Console.WriteLine(name);
+
+//var field = Caller.GetField(example);
+//Console.WriteLine(field);
+
+//var propertyGet = Caller.GetProperty(example);
+//Console.WriteLine(propertyGet);
+
+public class Caller
+{
+    public static string PublicMethod()
+    {
+        return "Public Hello World";
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Method")]
+    public static extern string CallMethod(Example example);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "Field")]
+    public static extern ref string GetField(Example example);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_Property")]
+    public static extern string GetProperty(Example example);
+}
+
+public class Example
+{
+
+    private string Field = "Taylor";
+
+    private string Method()
+    {
+        return "Hello World";
+    }
+
+    private string Property { get; set; } = "Taylor";
+}
